92a656580153bfbe6224a122db79ebd3d8bf938a
nelnawawy-ventiv <82230176+nelnawawy-ventiv@users.noreply.github.com>
2021-07-23 11:38:04 -0400

    Create baseproperties2.js

---
+++ baseproperties2.js
@@ -0,0 +1,175 @@
+define(["https://api.mapbox.com/mapbox-gl-js/v2.3.1/mapbox-gl.js", "jquery", "https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-geocoder/v4.7.0/mapbox-gl-geocoder.min.js", "https://api.mapbox.com/mapbox.js/plugins/turf/v3.0.11/turf.min.js", "https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-draw/v1.3.0/mapbox-gl-draw.js", "https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js",/*"https://code.jquery.com/ui/1.12.1/jquery-ui.min.js",*/ "text! https://cdn.jsdelivr.net/gh/nelnawawy-ventiv/geotest/Demobase7.css"], function (mapboxgl, jQuery, MapboxGeocoder, turf, MapboxDraw, slider1, slider2, Info) {
+    "use strict";
+    var map = '',
+        bounds = '',
+        geojsonFeature = {};
+    var property = 'visible';
+
+
+    function BasicControl() { };
+    BasicControl.prototype.initialize = function (oControlHost, fnDoneInitializing, oDataStore) {
+
+        document.getElementById("v30_ValueComboBox").addEventListener("change", myFunction);
+
+        function myFunction() {
+
+            var visibility = map.getLayoutProperty('Properties2', 'visibility');
+
+            if (sessionStorage.getItem('visibility') === 'visible') {
+                map.setLayoutProperty('Properties2', 'visibility', 'none');
+                sessionStorage.setItem('visibility', 'none');
+                //document.getElementById("v30_ValueComboBox").style.backgroundColor = '#9BA3A5';
+
+            } else {
+
+                map.setLayoutProperty('Properties2', 'visibility', 'visible');
+                sessionStorage.setItem('visibility', 'visible');
+                //document.getElementById("v30_ValueComboBox").style.backgroundColor = '#EFE013';
+            }
+        }
+
+        jQuery("head link[rel='stylesheet']").last().after("<link href='https://api.mapbox.com/mapbox-gl-js/v2.3.1/mapbox-gl.css' rel='stylesheet' />");
+        //jQuery("head link[rel='stylesheet']").last().after("<link href='https://code.jquery.com/ui/1.12.1/themes/smoothness/jquery-ui.css' />");
+        jQuery("body").after("<link rel='stylesheet' href='https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-geocoder/v4.7.0/mapbox-gl-geocoder.css' type='text/css' />");
+        jQuery("body").prepend("<link rel='stylesheet' href='https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-draw/v1.3.0/mapbox-gl-draw.css' type='text/css'/>");
+        jQuery("tbody").after("<link rel='stylesheet' href='https://cdn.jsdelivr.net/gh/nelnawawy-ventiv/geotest/Demobase7.css' rel='stylesheet' />");
+        jQuery("head link[rel='stylesheet']").last().after("<link href='https://cdn.jsdelivr.net/gh/nelnawawy-ventiv/geotest/Demobase7.css' type='text/css' />");
+
+        //jQuery("td style").prepend("<div id='map'></div><div class='calculation-box'><p>Draw a polygon using the draw tools.</p><div id='calculated-area'></div></div>");
+        var mapContainer = oControlHost.container.id;
+
+        //*** Step 2a make some minor adjustments to default map */
+        mapboxgl.accessToken = 'pk.eyJ1IjoiZnhoYXdrIiwiYSI6ImNqaDZqYmVsajFwb3kycWs0dzM5aDFxbXgifQ.DcqavEFQJWPJ8eUAGLbK_A'; //Make sure to add Map Token Key
+        map = new mapboxgl.Map({
+            container: mapContainer,
+            style: 'mapbox://styles/mapbox/dark-v10',
+            center: [-.165748, 51.507], //Center Map
+            zoom: 5, //Change Default Zoom
+            interactive: true //Set Interactive to true
+        });
+
+        //Set up the Bounds variable
+        bounds = new mapboxgl.LngLatBounds();
+        /*
+        var layerList = document.getElementById('menu');
+        var inputs = layerList.getElementsByTagName('input');
+
+        function switchLayer(layer) {
+            var layerId = layer.target.id;
+            map.setStyle('mapbox://styles/mapbox/' + layerId);
+        }
+
+        for (var i = 0; i < inputs.length; i++) {
+            inputs[i].onclick = switchLayer;
+        }
+        */
+        //Tell Cognos that we are done initializing 
+        fnDoneInitializing();
+
+    };
+
+    BasicControl.prototype.draw = function (oControlHost) {
+
+        console.log('3. Draw ******************')
+
+        map.on('load', function () {
+            map.addSource('Properties2', {
+                type: 'vector',
+                url: 'mapbox://fxhawk.cjhkjvk4202mi6mt2y4lkns44-7mdr6'
+            });
+            map.addLayer({
+                'id': 'Properties2',
+                'type': 'symbol',
+                'source': 'Properties2',
+                'source-layer': 'Properties2',
+                "layout": {
+                    "visibility": property,
+                    "text-field": [
+                        "to-string",
+                        [
+                            "get",
+                            "Office"
+                        ]
+                    ],
+                    "text-size": 12,
+                    "icon-image": "town-hall-11",
+                    "text-anchor": "top-left",
+                    "icon-size": 1
+                },
+                "paint": {
+                    "text-color": "rgb(152, 153, 29)", //(187,189,11)", //"rgb(234, 236, 24)",
+                    "icon-opacity": 0.8
+                }
+            });
+        })
+
+        map.on('click', function (e) {
+            var features = map.queryRenderedFeatures(e.point, {
+                layers: ['Properties2'] // replace this with the name of the layer
+            });
+
+            if (!features.length) {
+                return;
+            }
+
+            var feature = features[0];
+            var url = 'http://www.ventivtech.com'
+            var popup = new mapboxgl.Popup({ offset: [0, -15] })
+                .setLngLat(feature.geometry.coordinates)
+                .setHTML('<h3>' + feature.properties.Office + '</h3>' + 'Street: ' + feature.properties.Street + '<br>' + 'Insured Value: ' + feature.properties.Insured_Value + '</p>')
+                .setLngLat(feature.geometry.coordinates)
+
+                .addTo(map);
+        });
+        // Change the cursor to a pointer when the mouse is over the places layer.
+        map.on('mouseenter', 'Properties2', function () {
+            map.getCanvas().style.cursor = 'pointer';
+        });
+
+        // Change it back to a pointer when it leaves.
+        map.on('mouseleave', 'Properties2', function () {
+            map.getCanvas().style.cursor = '';
+        });
+
+    };
+
+    BasicControl.prototype.setData = function (oControlHost, oDataStore) {
+        console.log('2 Set Data')
+        //Default GeoJSON
+        geojsonFeature = {
+            "type": "FeatureCollection",
+            "features": []
+        }
+
+        var iRowCount = oDataStore.rowCount;
+
+        for (var iRow = 0; iRow < iRowCount; iRow++) {
+
+            var feature = {}
+            feature['type'] = 'Feature'
+            feature['geometry'] = {
+                'type': 'Point',
+                'coordinates': [parseFloat(oDataStore.getCellValue(iRow, 1)), parseFloat(oDataStore.getCellValue(iRow, 2))],
+            }
+            feature['properties'] = { 'name': oDataStore.getCellValue(iRow, 0), 'total_paid3': oDataStore.getCellValue(iRow, 15) }
+            //earth = oDataStore.getCellValue(iRow, 3)
+            //radar = oDataStore.getCellValue(iRow, 4)
+            //MyStyle = oDataStore.getCellValue(iRow, 5)
+            property = oDataStore.getCellValue(iRow, 6)
+            //lightning = oDataStore.getCellValue(iRow, 7)
+            //hurricane = oDataStore.getCellValue(iRow, 8)
+            //hazardous = oDataStore.getCellValue(iRow, 9)
+            //earthquake_h = oDataStore.getCellValue(iRow, 10)
+            //tornadoes = oDataStore.getCellValue(iRow, 11)
+            //claims = oDataStore.getCellValue(iRow, 12)
+            //claim_info = oDataStore.getCellValue(iRow, 13)
+           // wildfire_p = oDataStore.getCellValue(iRow, 14)
+
+
+
+            geojsonFeature['features'].push(feature)
+        }
+    };
+
+    return BasicControl;
+});
